49bb93fa74a6461c33194333a24582534211e74c
MauGuerrero <102774770+Mauricio-Guerrero-Arciniegas@users.noreply.github.com>
2025-08-17 16:38:07 -0500

    title-animation

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,36 @@ import { motion } from 'framer-motion';
 import styles from './styles/home.module.scss';
 import { useLanguage } from '@/context/LanguageContext';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function HomePage() {
   const { t } = useLanguage();
+  const title = t.home.title; // "Mauricio Guerrero A."
+  const [finished, setFinished] = useState(false);
 
   return (
     <main className={styles.home}>
       <motion.section
-        className={styles.home__hero}
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
+        className={styles.home__glass}
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.6 }}
       >
-        <h1 className={styles.home__title}>{t.home.title}</h1>
+        <h1
+          className={`${styles.home__title} ${
+            finished ? styles['home__title--final'] : ''
+          }`}
+        >
+          <motion.span
+            initial={{ backgroundPosition: '200% 50%' }}
+            animate={{ backgroundPosition: '0% 50%' }}
+            transition={{ duration: 2, ease: 'easeInOut' }}
+            onAnimationComplete={() => setFinished(true)}
+          >
+            {title}
+          </motion.span>
+        </h1>
+
         <p className={styles.home__text}>{t.home.subtitle}</p>
 
         <div className={styles.home__buttons}>
@@ -31,6 +48,8 @@ export default function HomePage() {
           </Link>
         </div>
       </motion.section>
+
+      <div className={styles.home__right}></div>
     </main>
   );
 }
